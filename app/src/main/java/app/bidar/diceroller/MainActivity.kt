0486a28492e45ae0932a739be184bc58547a3952
rezInHome <rezajaxg@gmail.com>
2022-05-13 01:32:13 +0430

    this is branch debug and test commit 2

--- app/src/main/java/app/bidar/diceroller/MainActivity.kt
+++ app/src/main/java/app/bidar/diceroller/MainActivity.kt
@@ -22,6 +22,7 @@ import app.bidar.diceroller.ui.theme.DiceRollerTheme
 
 
 // this is branch debug
+// this is branch debug and test commit 2
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
